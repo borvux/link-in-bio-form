02152c68b661302486977c0e000b9a8e7b5ef774
borvux <bennyjoram@gmail.com>
2025-05-03 00:08:39 -0500

    solution

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,18 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def form
+    render({ :template => "item_templates/form" })
+  end
+
+  def create
+    new_item = Item.new
+    new_item.link_url = params.fetch("link_url")
+    new_item.link_description = params.fetch("link_description")
+    new_item.thumbnail_url = params.fetch("thumbnail_url")
+    new_item.save
+
+    redirect_to("/")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-
   get("/", { :controller => "items", :action => "index" })
+  get("/backdoor", { :controller => "items", :action => "form" })
+  post("/insert_item", { :controller => "items", :action => "create" })
 end
